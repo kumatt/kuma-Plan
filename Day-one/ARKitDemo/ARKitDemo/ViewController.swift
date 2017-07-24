43f57b05ed6b7068b5e9a11585b883f47b912588
Blanbok <a1653913692@icloud.com>
2017-07-24 19:24:47 +0800

    small change

--- Day-one/ARKitDemo/ARKitDemo/ViewController.swift
+++ Day-one/ARKitDemo/ARKitDemo/ViewController.swift
@@ -23,11 +23,6 @@ class ViewController: UIViewController,ARSCNViewDelegate {
         return scnView
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.scnView.session.run(ARWorldTrackingSessionConfiguration.init())
